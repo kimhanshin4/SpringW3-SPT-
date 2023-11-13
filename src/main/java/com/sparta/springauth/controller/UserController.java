9e4cf28be23d2df2093b8432d971bcc2300097de
kimhanshin4 <funold1004@gmail.com>
2023-11-13 17:32:44 +0900

    Spring_W3 - 로그인 구현 : JWT 아 머리터지겠!!!

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sparta.springauth.controller;
 
 import com.sparta.springauth.dto.*;
 import com.sparta.springauth.service.*;
+import jakarta.servlet.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +30,16 @@ public class UserController {
     public String signup(SignupRequestDto requestDto) {
         userService.signup(requestDto);
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error"; //?=>오류시 이렇게 요청하게 해달라 Client라는 요구사항
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -1,9 +1,11 @@
 package com.sparta.springauth.service;
 
-import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.dto.*;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.jwt.*;
 import com.sparta.springauth.repository.UserRepository;
+import jakarta.servlet.http.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder,
+        JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     // ADMIN_TOKEN
@@ -55,5 +60,24 @@ public class UserService {
         //DB의 한 row == 해당 Entity Class의 한 객체
         User user = new User(username, password, email, role);
         userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        //DB에 사용자가 존재하는지 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+            () -> new IllegalArgumentException("누구세요 휴먼?")
+        );
+
+        //비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 다르잖아요!");
+        }
+
+        //인증완료 => JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token, res);
     }
 }
